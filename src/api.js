2f8b89ff697bec794aaab1f1a398d1aa0a33bafb
Vishnu Thiyagarajan <Monocept@MONO-HYD-LAP136.corp.monocept.com>
2021-09-07 15:38:34 +0530

    Initial setup

---
+++ src/api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!",
+  });
+});
+
+router.get("/test", (req, res) => {
+  res.json({
+    test: "tested",
+  });
+});
+
+app.use("/.netlify/functions/api", router);
+
+module.exports.handler = serverless(app);
