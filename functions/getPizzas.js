dde42221cd7f5a3de0bf20e590bb0191950905c4
Vishnu Thiyagarajan <Monocept@MONO-HYD-LAP136.corp.monocept.com>
2021-09-10 16:52:59 +0530

    common headers

--- functions/getPizzas.js
+++ functions/getPizzas.js
@@ -1,11 +1,16 @@
 import { connectToDatabase } from "../lib/database";
+import { headers } from "../util/headers";
 
 exports.handler = async (event, context) => {
   if (event.httpMethod === "GET") {
     const db = await connectToDatabase();
     const collection = await db.collection("Pizza");
     const pizzas = await collection.find({}).toArray();
-    return { statusCode: 200, body: JSON.stringify({ pizzas }) };
+    return {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify({ pizzas }),
+    };
   }
   return { statusCode: 400, body: JSON.stringify({}) };
 };
---
+++ util/headers.js
@@ -0,0 +1,7 @@
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+};
+
+export default headers;
